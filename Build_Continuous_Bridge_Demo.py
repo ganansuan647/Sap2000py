1f0b9c7e8ca82a44f1134a2e1ccab499accfa697
ganansuan647 <1852992@tongji.edu.cn>
2024-07-25 09:50:19 +0800

    add feature:extract polygon from dxf

--- Build_Continuous_Bridge_Demo.py
+++ Build_Continuous_Bridge_Demo.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from loguru import logger
 from pathlib import Path
 from collections import namedtuple
+from extract_polygon_from_dxf import DXF2Polygons
+from functools import lru_cache
 
 #full path to the model
 ModelPath = Path('.\Test\Test.sdb')
@@ -58,16 +60,30 @@ class Section_General:
     def get_elements_with_this_section(self):
         raise NotImplementedError
 
-# boxsection = Section_General(
-#     name = "#468_pier_top_solid",
-#     material = "C40",
-#     Area = 100,
-#     Depth = 24,Width = 14,
-#     As2=100,As3=100,
-#     I22=80,I33=80,J=100,
-#     notes = "#468_pier_top_solid_section")
-# ret = boxsection.define()
-# print(ret)
+# define a general frame section
+H50sec = DXF2Polygons(file_path=r'Test\TongZhouSha_H_above_50.dxf', unit_of_dxf='cm', show_log=False)
+H45sec = DXF2Polygons(file_path=r'Test\TongZhouSha_H_40_to_50.dxf', unit_of_dxf='cm', show_log=False)
+H40sec = DXF2Polygons(file_path=r'Test\TongZhouSha_H_below_40.dxf', unit_of_dxf='cm', show_log=False)
+solid_section = Section_General(
+    name = "#468_pier_solid",
+    material = "C40",
+    Area = H50sec.outer_geometric_properties['area'],
+    Depth = H50sec.outer_geometric_properties['height'],Width = H50sec.outer_geometric_properties['width'],
+    As2=H50sec.outer_geometric_properties['Asy'],As3=H50sec.outer_geometric_properties['Asx'],
+    I22=H50sec.outer_geometric_properties['Iyy'],I33=H50sec.outer_geometric_properties['Ixx'],J=H50sec.outer_geometric_properties['J'],
+    notes = "#468_pier_top_solid_section")
+ret = solid_section.define()
+print(ret)
+box_section = Section_General(
+    name = "#468_pier_box",
+    material = "C40",
+    Area = H50sec.combine_geometric_properties['area'],
+    Depth = H50sec.combine_geometric_properties['height'],Width = H50sec.combine_geometric_properties['width'],
+    As2=H50sec.combine_geometric_properties['Asy'],As3=H50sec.combine_geometric_properties['Asx'],
+    I22=H50sec.combine_geometric_properties['Iyy'],I33=H50sec.combine_geometric_properties['Ixx'],J=H50sec.combine_geometric_properties['J'],
+    notes = "#468_pier_top_solid_section")
+ret = box_section.define()
+print(ret)
 
 @dataclass
 class SapPoint:
@@ -75,22 +91,29 @@ class SapPoint:
     y: float
     z: float
     name: str = ""
+    mass: float = 0.0
     
     def add(self):
         ret = Sap.Assign.PointObj.AddCartesian(self.x, self.y, self.z, UserName=self.name)
         if ret == 0:
             logger.success(f"Point {self.name} : ({self.x}, {self.y}, {self.z}) added successfully.")
         return ret
+    
+    def defineMass(self):
+        ret = Sap.Assign.PointObj.Set.Mass(self.name, [self.mass for i in range(6)])
+        if ret == 0:
+            logger.success(f"Mass {self.mass} added to Point {self.name}")
+        return ret
 
 @dataclass
-class SapBase_6Spring:
+class SapBase_6Spring():
     def __init__(self, point: SapPoint, pier_name, spring_data_name=None, **kwargs):
         self.point = point
         self.pier_name = pier_name
         self.name = self.pier_name + "_Base"
         self.point.name = self.name
         if not spring_data_name:
-            self.spring_data_name = self.pier_name.split("#")[-1]
+            self.spring_data_name = self.pier_name.split("_")[0].split("#")[-1]
         else:
             self.spring_data_name = spring_data_name
     
@@ -145,78 +168,207 @@ class SapBase_6Spring:
 @dataclass
 class SapPier_Box:
     name: str
+    station: float  # station of the pier, like K100+000 -> 100000
+    horizontal_pos: float
     Height_of_pier_bottom: float
     Height_of_pier: float
+    Height_of_cap: float
+    Mass_of_cap_in_ton: float
     bottom_solid_length: float
-    bottom_solid_section: tuple
     top_solid_length: float
-    top_solid_section: tuple
-    hollow_excavation: tuple
-    num_of_hollow: int
+    Distance_between_bearings: float
+    num_of_hollow_elements: int =1
+    is_intermediate_pier: bool =False # whether the pier is an intermediate pier, if true, the pier will have 4 bearings otherwise 2
+    offset: float =1.0 # the distance between the centerline of the support (bearing) and the centerline of the pier.
     
     def __post_init__(self):
         self.generate_points()
-        self.generate_elements()
+        self.generate_pier_elements()
+        # self.add_body_constraint()
+        self.add_mass()
+        
+        self.base.get_spring_data()
+        self.base.add_spring()
+    
+    def add_mass(self):
+        self.cap_point.defineMass()
     
     def generate_points(self):
-        try:
-            self.pier_hollow_bottom = SapPoint(self.pier_bottom.x, self.pier_bottom.y, self.Height_of_pier_bottom + self.bottom_solid_length, f"{self.name}_HollowBottom")
-            self.pier_hollow_top = SapPoint(self.pier_bottom.x, self.pier_bottom.y, self.Height_of_pier_bottom + self.Height_of_pier - self.top_solid_length, f"{self.name}_HollowTop")
-            self.pier_top = SapPoint(self.pier_bottom.x, self.pier_bottom.y, self.Height_of_pier_bottom + self.Height_of_pier, f"{self.name}_Top")
-            self.pier_hollow_points = [SapPoint(self.pier_bottom.x, self.pier_bottom.y, self.Height_of_pier_bottom + self.bottom_solid_length + i*(self.Height_of_pier - self.bottom_solid_length - self.top_solid_length)/self.num_of_hollow, f"{self.name}_Hollow_{i+1}") for i in range(self.num_of_hollow)]
+        x,y = self.station,self.horizontal_pos
+        points = []
+        # base point
+        self.base_point = SapPoint(x, y, self.Height_of_pier_bottom-self.Height_of_cap, f"{self.name}_Base")
+        self.base = SapBase_6Spring(self.base_point, self.name)
+        points.append(self.base_point)
+        
+        # cap points
+        self.cap_point = SapPoint(x, y, self.Height_of_pier_bottom-self.Height_of_cap/2, f"{self.name}_Cap",mass=self.Mass_of_cap_in_ton)
+        points.append(self.cap_point)
+        
+        # pier points
+        self.pier_bottom_point = SapPoint(x, y, self.Height_of_pier_bottom, f"{self.name}_Bottom")
+        points.append(self.pier_bottom_point)
+        self.pier_hollow_bottom = SapPoint(x, y, self.Height_of_pier_bottom + self.bottom_solid_length, f"{self.name}_HollowBottom")
+        points.append(self.pier_hollow_bottom)
+        hollow_points = []
+        for i,h in enumerate(self.__hollow_points_to_define()):
+            middle_point = SapPoint(x, y, h, f"{self.name}_HollowMiddle_{i}")
+            points.append(middle_point)
+            hollow_points.append(middle_point)
+        self.hollow_points=hollow_points
+        self.pier_hollow_top = SapPoint(x, y, self.Height_of_pier_bottom + self.Height_of_pier - self.top_solid_length, f"{self.name}_HollowTop")
+        points.append(self.pier_hollow_top)
+        self.pier_top = SapPoint(x, y, self.Height_of_pier_bottom + self.Height_of_pier, f"{self.name}_Top")
+        
+        # bearing bottom points
+        if y>0:
+            y_outer = y + self.Distance_between_bearings/2
+            y_inner = y - self.Distance_between_bearings/2
+        else:
+            y_outer = y - self.Distance_between_bearings/2
+            y_inner = y + self.Distance_between_bearings/2
             
-            # Add points to SAP2000 model
-            self.pier_bottom.add()
-            self.pier_hollow_bottom.add()
-            self.pier_hollow_top.add()
-            self.pier_top.add()
-            for point in self.pier_hollow_points:
-                point.add()
-        except Exception as e:
-            logger.error(f"Error in generating points for {self.name}: {e}")
+        if self.is_intermediate_pier:
+            self.bearing_bottom_point_outer = [SapPoint(x-self.offset, y_outer, self.Height_of_pier_bottom, f"{self.name}_BearingBottom_outter_1"),
+                                               SapPoint(x+self.offset, y_outer, self.Height_of_pier_bottom, f"{self.name}_BearingBottom_outter_2")]
+            self.bearing_bottom_point_inner = [SapPoint(x-self.offset, y_inner, self.Height_of_pier_bottom, f"{self.name}_BearingBottom_inner_1"),
+                                               SapPoint(x+self.offset, y_inner, self.Height_of_pier_bottom, f"{self.name}_BearingBottom_inner_2")]
+            points.extend(self.bearing_bottom_point_outer)
+            points.extend(self.bearing_bottom_point_inner)
+        else: 
+            self.bearing_bottom_point_outer = SapPoint(x, y_outer, self.Height_of_pier_bottom, f"{self.name}_BearingBottom_outter")
+            self.bearing_bottom_point_inner = SapPoint(x, y_inner, self.Height_of_pier_bottom, f"{self.name}_BearingBottom_inner")
+            points.append(self.bearing_bottom_point_outer)
+            points.append(self.bearing_bottom_point_inner)
+        
+        self.points = points
+        # Add points to SAP2000 model
+        for point in self.points:
+            point.add()
 
-    def generate_elements(self):
-        try:
-            # Generate bottom solid section element
-            Sap.Assign.FrameObj.AddByCoord(self.pier_bottom.x, self.pier_bottom.y, self.pier_bottom.z, self.pier_hollow_bottom.x, self.pier_hollow_bottom.y, self.pier_hollow_bottom.z, f"{self.name}_BottomSolid")
-            Sap.PropFrame.SetRectangle(f"{self.name}_BottomSolid", "Concrete", self.bottom_solid_section[0], self.bottom_solid_section[1])
+    def __hollow_points_to_define(self):
+        coord_list = [self.Height_of_pier_bottom + self.bottom_solid_length, self.Height_of_pier_bottom + self.Height_of_pier - self.top_solid_length]
+        if self.num_of_hollow_elements == 1:
+            return []
+        elif self.num_of_hollow_elements > 1:
+            pass
+        else:
+            logger.error("Number of hollow elements is not valid.")
+            return None
 
-            # Generate top solid section element
-            Sap.Assign.FrameObj.AddByCoord(self.pier_hollow_top.x, self.pier_hollow_top.y, self.pier_hollow_top.z, self.pier_top.x, self.pier_top.y, self.pier_top.z, f"{self.name}_TopSolid")
-            Sap.PropFrame.SetRectangle(f"{self.name}_TopSolid", "Concrete", self.top_solid_section[0], self.top_solid_section[1])
+        result = []
+        # interpolate the height of hollow points
+        for i in range(1, self.num_of_hollow_elements):
+            result.append(coord_list[0] + (coord_list[1] - coord_list[0]) * i / self.num_of_hollow_elements)
+        return result
 
-            # Generate hollow section elements
-            for i in range(len(self.pier_hollow_points) - 1):
-                Sap.Assign.FrameObj.AddByCoord(self.pier_hollow_points[i].x, self.pier_hollow_points[i].y, self.pier_hollow_points[i].z, self.pier_hollow_points[i + 1].x, self.pier_hollow_points[i + 1].y, self.pier_hollow_points[i + 1].z, f"{self.name}_Hollow_{i+1}")
-                Sap.PropFrame.SetCircle(f"{self.name}_Hollow_{i+1}", "Concrete", self.hollow_excavation[0], self.hollow_excavation[1])
-
-        except Exception as e:
-            logger.error(f"Error in generating elements for {self.name}: {e}")
-
+    # @lru_cache(maxsize=3)
+    def get_solid_section(self):
+        if self.Height_of_pier >= 50:
+            sec = DXF2Polygons(file_path=r'Test\TongZhouSha_H_above_50.dxf', unit_of_dxf='cm', show_log=False)
+        elif self.Height_of_pier >= 40:
+            sec = DXF2Polygons(file_path=r'Test\TongZhouSha_H_40_to_50.dxf', unit_of_dxf='cm', show_log=False)
+        elif self.Height_of_pier < 40 and self.Height_of_pier >= 0:
+            sec = DXF2Polygons(file_path=r'Test\TongZhouSha_H_below_40.dxf', unit_of_dxf='cm', show_log=False)
+        else:
+            logger.error("Height of pier is not valid.")
+            return None
+        solid_section = Section_General(
+            name = self.name+"_pier_solid",
+            material = "C40",
+            Area = sec.outer_geometric_properties['area'],
+            Depth = sec.outer_geometric_properties['height'],Width = sec.outer_geometric_properties['width'],
+            As2=sec.outer_geometric_properties['Asy'],As3=sec.outer_geometric_properties['Asx'],
+            I22=sec.outer_geometric_properties['Iyy'],I33=sec.outer_geometric_properties['Ixx'],J=sec.outer_geometric_properties['J'],
+            notes = self.name+"_pier_solid_section")
+        return solid_section
+    
+    # @lru_cache(maxsize=3)
+    def get_box_section(self):
+        if self.Height_of_pier >= 50:
+            sec = DXF2Polygons(file_path=r'Test\TongZhouSha_H_above_50.dxf', unit_of_dxf='cm', show_log=False)
+        elif self.Height_of_pier >= 40:
+            sec = DXF2Polygons(file_path=r'Test\TongZhouSha_H_40_to_50.dxf', unit_of_dxf='cm', show_log=False)
+        elif self.Height_of_pier < 40 and self.Height_of_pier >= 0:
+            sec = DXF2Polygons(file_path=r'Test\TongZhouSha_H_below_40.dxf', unit_of_dxf='cm', show_log=False)
+        else:
+            logger.error("Height of pier is not valid.")
+            return None
+        box_section = Section_General(
+            name = self.name+"_pier_box",
+            material = "C40",
+            Area = sec.combine_geometric_properties['area'],
+            Depth = sec.combine_geometric_properties['height'],Width = sec.combine_geometric_properties['width'],
+            As2=sec.combine_geometric_properties['Asy'],As3=sec.combine_geometric_properties['Asx'],
+            I22=sec.combine_geometric_properties['Iyy'],I33=sec.combine_geometric_properties['Ixx'],J=sec.combine_geometric_properties['J'],
+            notes = self.name+"_pier_box_section")
+        return box_section
+    
+    def get_cap_section(self):
+        width = 12 if self.Height_of_pier >= 50 else 10.794
+        depth = 18.625
+        ret = Sap._Model.PropFrame.SetRectangle(self.name+"_Cap", "C30", width, depth)
+        if ret == 0:
+            logger.success(f"Cap section {self.name}_Cap added!")
+        return self.name+"_Cap"
+    
+    def add_body_constraint(self):
+        Sap.Define.jointConstraints.SetBody(self.pier_top.name, True, True, True, True, True, True)
+        Sap.Define.jointConstraints.SetDiaphragm(self.pier_top.name, True, True, True)
+        ret = Sap.Assign.PointObj.SetRestraint(self.base_point.name, [True, True, True, True, True, True])
+        if ret[1] == 0:
+            logger.success(f"Body constraint added to {self.base_point.name}")
+        return ret
+    
+    def generate_pier_elements(self):
+        solid_section = self.get_solid_section()
+        box_section = self.get_box_section()
+        solid_section.define()
+        box_section.define()
+        cap_section_name = self.get_cap_section()
+        
+        # define solid cap
+        Sap.Assign.FrameObj.AddByPoint(self.base_point.name, self.cap_point.name, propName=cap_section_name, userName = self.name+"base2cap")
+        Sap.Assign.FrameObj.AddByPoint(self.cap_point.name, self.pier_bottom_point.name, propName=cap_section_name, userName = self.name+"cap2bottom")
+        
+        # define pierpython
+        Sap.Assign.FrameObj.AddByPoint(self.pier_bottom_point.name, self.pier_hollow_bottom.name, propName=solid_section.name, userName = self.name+"bottom2hollowBottom")
+        for i,h in enumerate(self.hollow_points):
+            if i == 0:
+                Sap.Assign.FrameObj.AddByPoint(self.pier_hollow_bottom.name, h.name, propName=box_section.name, userName = self.name+f"hollow_{i+1}")
+            else:
+                Sap.Assign.FrameObj.AddByPoint(self.hollow_points[i-1].name, h.name, propName=box_section.name, userName = self.name+f"hollow_{i+1}")
+        if len(self.hollow_points) == 0:
+            Sap.Assign.FrameObj.AddByPoint(self.pier_hollow_bottom.name, self.pier_hollow_top.name, propName=box_section.name, userName = self.name+"hollowBottom2Top")
+        else:
+            Sap.Assign.FrameObj.AddByPoint(self.hollow_points[-1].name, self.pier_hollow_top.name, propName=box_section.name, userName = self.name+f"hollow_{i+1}")
+        Sap.Assign.FrameObj.AddByPoint(self.pier_hollow_top.name, self.pier_top.name, propName=solid_section.name, userName = self.name+"hollowTop2Top")
 
 sap_pier = SapPier_Box(
     name="#468",
+    station=0,
+    horizontal_pos=8.125,
     Height_of_pier_bottom=-3.1,
     Height_of_pier=40.0,
+    Height_of_cap=3.0,
+    Mass_of_cap_in_ton=1676.25,
     bottom_solid_length=2.0,
-    bottom_solid_section_shape=(9.0,4.0),
     top_solid_length=3.0,
-    top_solid_section_shape=(10.6, 4.0),
-    hollow_excavation=(1.5, 1.5),
-    num_of_hollow=2,
-    bottom_solid_section_prop=(9.0, 4.0),
-    top_solid_section_prop=(10.6, 4.0),
+    Distance_between_bearings=7.0,
+    num_of_hollow_elements=2,
+    is_intermediate_pier = True,
+    offset = 1.0
 )
     
-    
+a=1
 
-point1 = SapPoint(*[1,0,0], "#468")
-base468 = SapBase_6Spring(point1, "#468")
+# point1 = SapPoint(*[1,0,0], "#468")
+# base468 = SapBase_6Spring(point1, "#468")
 # use auto_build to add point and spring or just do it step by step
 # base468.add_point()
 # base468.get_spring_data()
 # base468.add_spring()
-base468.auto_build()
+# base468.auto_build()
 
 # ret = Sap.Assign.PointObj.Get.CoordCartesian(name)
 # print(ret)
---
+++ extract_polygon_from_dxf.py
@@ -0,0 +1,259 @@
+import ezdxf
+from shapely.geometry import Polygon
+import numpy as np
+import math
+
+class DXF2Polygons:
+    """
+    read dxf file and extract polygons, output unit is meter
+    """
+    def __init__(self, file_path, unit_of_dxf='m', show_log=False):
+        if show_log:
+            from loguru import logger
+            self.log = True
+            self.logger = logger.bind(name='DXF2Polygons')
+        else:
+            self.log = False
+        self.file_path = file_path
+        self.unit_of_dxf = unit_of_dxf
+        # 提取封闭图形(提取后单位为m)
+        self.polygons = self.get_polygons_from_dxf(file_path, unit_of_dxf)
+        # 分类外侧边界和内侧边界
+        outer_boundaries, inner_boundaries = self.classify_polygons(self.polygons)
+        self.outer_boundaries = outer_boundaries
+        self.inner_boundaries = inner_boundaries
+        # 合并外边界和内边界的几何性质
+        if len(outer_boundaries) == 1:
+            if self.log:
+                self.logger.success(f'合并后的几何属性（几何单位：m）:{self.combine_geometric_properties}')
+        else:
+            if self.log:
+                self.logger.warning('发现多个外侧边界，无法合并几何属性')
+
+    def plot(self):
+        import matplotlib.pyplot as plt
+        import geopandas as gpd
+        p = gpd.GeoSeries(self.combined_polygon)
+        p.plot()
+        plt.show()    
+
+    def get_geometry_from_entity(self,entity):
+        """从实体中获取几何信息"""
+        if entity.dxftype() == 'LWPOLYLINE' or entity.dxftype() == 'POLYLINE':
+            points = []
+            if entity.dxftype() == 'LWPOLYLINE':
+                # explode() 方法将多段线分解为基本图元
+                for sub_entity in entity.explode():
+                    if sub_entity.dxftype() == 'LINE':
+                        start_point = (sub_entity.dxf.start.x, sub_entity.dxf.start.y)
+                        end_point = (sub_entity.dxf.end.x, sub_entity.dxf.end.y)
+                        # 去重添加(距离小于0.1mm的点认为是同一个点)
+                        if len(points) == 0 or np.linalg.norm(np.array(points[-1]) - np.array(start_point)) > 0.01:
+                            points.append(start_point)
+                        if np.linalg.norm(np.array(start_point) - np.array(end_point)) > 0.01:
+                            points.append(end_point)
+                    elif sub_entity.dxftype() == 'ARC':
+                        center = (sub_entity.dxf.center.x, sub_entity.dxf.center.y)
+                        radius = sub_entity.dxf.radius
+                        start_angle = math.radians(sub_entity.dxf.start_angle)
+                        end_angle = math.radians(sub_entity.dxf.end_angle)
+                        arc_points = self.arc_to_points(center, radius, start_angle, end_angle)
+                        for arc_point in arc_points:
+                            if len(points) == 0 or np.linalg.norm(np.array(points[-1]) - np.array(arc_point)) > 0.01:
+                                points.append(arc_point)
+            else:  # POLYLINE
+                for vertex in entity.vertices:
+                    x, y = vertex.dxf.location.x, vertex.dxf.location.y
+                    points.append((x, y))
+            return points
+        elif entity.dxftype() == 'LINE':
+            start_point = (entity.dxf.start.x, entity.dxf.start.y)
+            end_point = (entity.dxf.end.x, entity.dxf.end.y)
+            return []
+        else:
+            raise ValueError(f"Unsupported entity type: {entity.dxftype()}")
+        
+    def arc_to_points(self,center, radius, start_angle, end_angle, num_points=40):
+        """将ARC转换为一系列点"""
+        cx, cy = center
+        if start_angle > end_angle:
+            end_angle += 2 * math.pi
+
+        angle_range = end_angle - start_angle
+        points = [
+            (
+                cx + radius * math.cos(start_angle + angle_range * (i / num_points)),
+                cy + radius * math.sin(start_angle + angle_range * (i / num_points))
+            )
+            for i in range(num_points + 1)
+        ]
+        return points
+        
+    def calculate_bulge(self,start_point, end_point, radius):
+        """计算弧的凸度"""
+        x1, y1 = start_point
+        x2, y2 = end_point
+        chord_length = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        sagitta = radius - math.sqrt(radius ** 2 - (chord_length / 2) ** 2)
+        return sagitta * 2 / chord_length
+
+    def bulge_to_arc(self,start_point, bulge, prev_point=None, points=None, index=None):
+        """将 bulge 转换为圆弧点集"""
+        arc_points = []
+        if prev_point is None:
+            arc_points.append(start_point)
+        else:
+            # Calculate arc center and radius
+            x1, y1 = prev_point
+            x2, y2 = start_point
+            angle = 4 * np.arctan(bulge)
+            chord_length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+            radius = chord_length / (2 * np.sin(angle / 2))
+
+            # Calculate start and end angles
+            start_angle = np.arctan2(y1 - y2, x1 - x2)
+            end_angle = start_angle + angle
+
+            # Calculate number of points on the arc
+            num_points = int(np.ceil(np.abs(angle) / (np.pi / 180)))
+
+            # Generate arc points
+            for i in range(num_points + 1):
+                theta = start_angle + (i / num_points) * (end_angle - start_angle)
+                x = x2 + radius * np.cos(theta)
+                y = y2 + radius * np.sin(theta)
+                arc_points.append((x, y))
+
+        return arc_points
+
+    def get_polygons_from_dxf(self,file_path, unit_of_dxf='m'):
+        """从 DXF 文件中提取封闭的多边形图形"""
+        doc = ezdxf.readfile(file_path)
+        msp = doc.modelspace()
+
+        polygons = []
+        entities = list(msp)  # 复制所有实体到一个列表中,以免炸开后msp发生变化
+        for entity in entities:
+            points = self.get_geometry_from_entity(entity)
+            if len(points) == 0:
+                continue
+            # Convert unit to m
+            if unit_of_dxf == 'cm':
+                points = [(x / 100, y / 100) for x, y in points]
+            elif unit_of_dxf == 'mm':
+                points = [(x / 1000, y / 1000) for x, y in points]
+            elif unit_of_dxf == 'm':
+                pass
+            else:
+                raise ValueError(f"Unsupported unit of dxf: {unit_of_dxf}")
+            
+            
+            if points:
+                # 去除重复点
+                unique_points = [points[i] for i in range(len(points)) if i == 0 or points[i] != points[i-1]]
+                polygon = Polygon(unique_points)
+                if polygon.is_valid:
+                    polygons.append(polygon)
+                    if self.log:
+                        self.logger.trace(f"Found a polygon with {len(unique_points)} points")
+                else:
+                    if self.log:
+                        self.logger.warning(f"Found an invalid polygon with {len(unique_points)} points\nInvalid points: {unique_points}")
+        return polygons
+
+    def classify_polygons(self,polygons):
+        """分类外侧边界和内侧边界"""
+        outer_boundaries = []
+        inner_boundaries = []
+
+        for polygon in polygons:
+            is_inner = False
+            for other_polygon in polygons:
+                if polygon != other_polygon and polygon.within(other_polygon):
+                    inner_boundaries.append(polygon)
+                    is_inner = True
+                    break
+            if not is_inner:
+                outer_boundaries.append(polygon)
+        
+        if self.log:
+            self.logger.info(f"Found {len(outer_boundaries)} outer boundaries and {len(inner_boundaries)} inner boundaries")
+
+        return outer_boundaries, inner_boundaries
+
+    def calculate_geometric_properties(self,polygon):
+        """计算几何属性：面积，特征尺寸，惯性矩和惯性积"""
+        area = polygon.area
+        bounds = polygon.bounds  # (minx, miny, maxx, maxy)
+        width = bounds[2] - bounds[0]
+        height = bounds[3] - bounds[1]
+
+        cx, cy = polygon.centroid.x, polygon.centroid.y
+
+        Ixx = 0
+        Iyy = 0
+        Ixy = 0
+
+        coords = list(polygon.exterior.coords)
+        for i in range(len(coords) - 1):
+            x0, y0 = coords[i]
+            x1, y1 = coords[i + 1]
+            a = x0 * y1 - x1 * y0
+            Ixx += (y0**2 + y0 * y1 + y1**2) * a
+            Iyy += (x0**2 + x0 * x1 + x1**2) * a
+            Ixy += (x0 * y1 + 2 * x0 * y0 + 2 * x1 * y1 + x1 * y0) * a
+
+        Ixx = abs(Ixx) / 12
+        Iyy = abs(Iyy) / 12
+        Ixy = abs(Ixy) / 24
+
+        J = Ixx + Iyy  # 转动惯量
+
+        # 计算剪切面积
+        Asx = area / (1 + (Ixx / (area * (height / 2)**2)))
+        Asy = area / (1 + (Iyy / (area * (width / 2)**2)))
+
+        properties = {
+            'area': area,
+            'width': width,
+            'height': height,
+            'centroid': (cx, cy),
+            'Ixx': Ixx, # I33
+            'Iyy': Iyy, # I22
+            'Ixy': Ixy,  # 惯性积I23
+            'J': J,  # 转动惯量
+            'Asx': Asx,  # 剪切面积 As3
+            'Asy': Asy  # 剪切面积 As2
+        }
+        
+        return properties
+
+    @property
+    def combine_geometric_properties(self):
+        """合并外边界和内边界的几何性质"""
+        return self.calculate_geometric_properties(self.combined_polygon)
+    
+    @property
+    def combined_polygon(self):
+        return Polygon(self.outer_polygon.exterior.coords, [inner.exterior.coords for inner in self.inner_polygons])
+    
+    @property
+    def outer_polygon(self):
+        return self.outer_boundaries[0]
+    
+    @property
+    def outer_geometric_properties(self):
+        """外边界的几何性质"""
+        return self.calculate_geometric_properties(self.outer_polygon)
+
+    @property
+    def inner_polygons(self):
+        return self.inner_boundaries
+
+if __name__ == '__main__':
+    # 示例文件路径
+    dxf_file_path = r'Test\TongZhouSha_H_above_50.dxf'
+    unit_of_dxf = 'cm'
+    show_log = True
+    H50section = DXF2Polygons(dxf_file_path, unit_of_dxf, show_log)
+    H50section.plot()
